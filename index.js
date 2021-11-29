74ae1f60dff389f683d950fde32fb09512eee474
Jason Botesazan <jbotesazan@gmail.com>
2021-11-29 10:04:04 -0500

    commit tree ab8a96222bade9b881c4f2603c73da69204cbec5

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const superbowlWin = function(item) {
 
+    let win = item.find( function(element) {
+        return element.result === "W"       
+     });
+
+    if (win === undefined) {
+        return;
+    }
+
+    return win.year
+}
